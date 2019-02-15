2377a63b8928d2a59c01d8ac57e5e671a5efdd94
김경민 <kyoungmin.kim@lgcns.com>
2019-02-15 20:37:11 +0900

    Initial commit

---
+++ StreetFighterIISpecialChampionEdition-Genesis/script.lua
@@ -0,0 +1,25 @@
+previous_agent_health = 176
+previous_cpu_health = 176
+
+function check_reward_by_health()
+  agent_health_delta = previous_agent_health - data.first_player_health
+  cpu_health_delta = previous_cpu_health - data.second_player_health
+  
+  previous_agent_health = data.first_player_health
+  previous_cpu_health = data.second_player_health
+    
+  return cpu_health_delta - agent_health_delta
+end
+
+function done_check()
+  if data.continuetimer == 0 then
+    return true
+  end
+  if data.first_player_matches_won == 1 then
+    return true
+  end
+  if data.second_player_matches_won == 1 then
+    return true
+  end
+  return false
+end
