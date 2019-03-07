fc40bc6aef7dab4b605c75c54624d42913f9d187
metsmania <kyoungmin.kim@lgcns.com>
2019-03-07 06:29:05 +0000

    script ceansed

---
+++ sf2/run.sh
@@ -0,0 +1,13 @@
+python small_network.py \
+    --rom_path '/root/sf2-workspace/sf2-env/StreetFighterIISpecialChampionEdition-Genesis' \
+    --model_path '/root/sf2-workspace/sf2-env/sf2/models/test/' \
+    --scenario 'scenario' \
+    --stack_size 5 \
+     --learning_rate 0.005 \
+    --total_episodes 10 \
+    --batch_size 64 \
+    --explore_start 1.0 \
+    --explore_stop 0.01 \
+    --decay_rate 0.00001 \
+    --gamma 0.95 \
+    --memory_size 100000
