b4180c2e12ec8d1c465b22ac38be49036dee6261
metsmania <kyoungmin.kim@lgcns.com>
2019-03-12 07:01:10 +0000

    with penalty
    Former-commit-id: 225b5fb544eb2ed187c2ecbd314a45226dcf27ff

---
+++ sf2/run_10000_time_penalty.sh
@@ -0,0 +1,13 @@
+python small_network_time_penalty.py \
+    --rom_path '/root/sf2-workspace/sf2-env/StreetFighterIISpecialChampionEdition-Genesis' \
+    --model_path '/root/sf2-workspace/sf2-env/sf2/models/64_nodes_time_penalty_10000_ep/' \
+    --scenario 'scenario' \
+    --stack_size 5 \
+     --learning_rate 0.005 \
+    --total_episodes 10000 \
+    --batch_size 64 \
+    --explore_start 1.0 \
+    --explore_stop 0.01 \
+    --decay_rate 0.00001 \
+    --gamma 0.95 \
+    --memory_size 100000
